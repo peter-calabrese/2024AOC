5464d6c175e5eb842d75d181701b46ba59fbb8ab
Peter Calabrese <peter@caldev.io>
2024-12-05 16:50:40 -0500

    Day 1 pt 2

--- Day1/index.js
+++ Day1/index.js
@@ -11,15 +11,33 @@ fs.readFile('input.txt', 'utf-8', (err, data) => {
     list1.push(num1);
     list2.push(num2);
   }
-  console.log(`Day 1 Answer: ${distance(list1, list2)}`);
+  console.log(`Part 1 Answer: ${distance(list1, list2)}`);
+  console.log(`Part 2 Answer: ${similarity(list1, list2)}`);
 });
 
 function distance(list1, list2) {
   list1.sort((a, b) => a - b);
   list2.sort((a, b) => a - b);
+  console.log(list1);
   let ans = 0;
   for (let i = 0; i < list1.length; i++) {
     ans += Math.max(list1[i], list2[i]) - Math.min(list1[i], list2[i]);
   }
+  return ans;
+}
+
+function similarity(list1, list2) {
+  let map = {};
+  let ans = 0;
+  for (const item of list1) {
+    if (!map[item]) map[item] = 0;
+  }
+  for (const item of list2) {
+    if (map[item] != undefined) map[item] = map[item] + 1;
+  }
+  Object.keys(map).map((el) => {
+    ans += parseInt(map[el]) * el;
+  });
+  console.log(map);
   return ans;
 }
